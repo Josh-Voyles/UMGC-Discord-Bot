520196db1d7590514303b4441bd2b057049d00ca
Josh Voyles <120749218+Josh-Voyles@users.noreply.github.com>
2024-03-09 12:29:22 -0500

    fixed hello name

--- umgc.bot.py
+++ umgc.bot.py
@@ -25,8 +25,9 @@ async def on_member_join(member):
 
 
 @bot.command(name='hello', help='Responds with Hello!')
-async def say_hello(ctx, member: discord.Member):
-    await ctx.send(f'Hello!, {member.mention}')
+async def say_hello(ctx):
+    m_name = ctx.author.display_name
+    await ctx.send(f'Hello!, {m_name}')
 
 
 @bot.command(name='github', help='Responds with its Github address.')
